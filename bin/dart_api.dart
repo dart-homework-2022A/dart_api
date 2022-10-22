d279eca71f3d5e6177cd9f2ee3fef6e3b9c712e4
ZARIFMAC <uzbzarif@gmail.com>
2022-10-22 15:18:53 +0500

    get response

--- bin/dart_api.dart
+++ bin/dart_api.dart
@@ -1,5 +1,7 @@
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
 
-void main() {
-  print('Hi');
+void main() async {
+  http.Response response = await http.get(
+    Uri.parse('https://www.google.com/'),
+  );
 }
