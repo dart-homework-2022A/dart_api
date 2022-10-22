f746b84b205a9df1542bcb9cc9d58906176a0fce
ZARIFMAC <uzbzarif@gmail.com>
2022-10-22 15:37:13 +0500

    get randomuser

--- bin/dart_api.dart
+++ bin/dart_api.dart
@@ -2,7 +2,7 @@ import 'package:http/http.dart' as http;
 
 void main() async {
   http.Response response = await http.get(
-    Uri.parse('http://diyorbekmajidov.pythonanywhere.com/'),
+    Uri.parse('https://randomuser.me/api/'),
   );
 
   print(response.body);
